0307230a0d3b3070417461a3171b4f959bc7548c
FinnArundel <68985116+FinnArundel@users.noreply.github.com>
2020-09-10 11:41:22 +1000

    Add files via upload

---
+++ week_06/sine_waves/sine_waves.js
@@ -0,0 +1,37 @@
+let x1;
+let y1;
+let x2;
+let y2;
+
+function setup() {
+  createCanvas (windowWidth, windowHeight);
+  background (0);
+}
+
+function draw() {
+
+  let x1 = sin(frameCount*0.04)*475;
+  let y1 = -sin(frameCount*0.07)*250;
+  let x2 = sin(frameCount*0.01)*600;
+  let y2 = -sin(frameCount*0.03)*350;
+
+  background (0,15);
+
+  noStroke();
+  noFill();
+  translate (width/2, height/2);
+  ellipse (x1, y1, 5);
+  ellipse (x2, y2, 5);
+
+  stroke (255);
+  line (x1, y1, x2, y2);
+}
+
+
+function keyPressed () {
+  noLoop(); 
+  } 
+ 
+function keyReleased (){
+    loop();
+}
