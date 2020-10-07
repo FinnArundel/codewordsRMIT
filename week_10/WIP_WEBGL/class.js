b1b11cb575073fdc1e190a40c6d67d5be6d82bb0
FinnArundel <68985116+FinnArundel@users.noreply.github.com>
2020-10-07 23:53:03 +1100

    Update class.js

--- week_10/WIP_WEBGL/class.js
+++ week_10/WIP_WEBGL/class.js
@@ -4,8 +4,9 @@ class NodePoint {
     this.y = y;
     this.z = z;
   }
-  
-    connect () {
+
+  connect () {
+    let time = timeSlider.value();
     for ( let i = 1; i < words.length; i++) {
       if (frameCount  > time * i + time) {
         stroke (250);
@@ -17,18 +18,18 @@ class NodePoint {
 
   show (count) {
     for (let i = 0; i < words.length; i++) {
- noStroke();
+      noStroke();
       if (count == 0) {
         fill (255, 0, 0);
-       // stroke (255, 0, 0);
+        // stroke (255, 0, 0);
       } else {
         fill (255);
         //stroke (255, 0, 0);
       }
-      
+
       textAlign (CENTER);
       textFont (helvetica, 12);
-      
+
       push();
       translate (this.x, this.y, this.z);
       let spin = radians(frameCount) *0.1;
