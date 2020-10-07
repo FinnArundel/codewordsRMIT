d26fc9f72d407c6477d7bd27ca1543652b7d0648
FinnArundel <68985116+FinnArundel@users.noreply.github.com>
2020-10-07 14:20:41 +1100

    Add files via upload

---
+++ week_10/matchbox_presentation/matchbox_presentation.js
@@ -0,0 +1,33 @@
+let matchbox;
+let matchImage;
+
+function preload() {
+  poster = loadImage ('data/poster.jpg');
+  matchImage = loadImage ('data/box.texture.png');
+  matchbox = loadModel ('data/matchbox.obj');
+}
+
+function setup() {
+createCanvas (windowWidth, windowHeight, WEBGL);
+  createEasyCam();
+  document.oncontextmenu = function() { 
+    return false;
+}
+}
+
+function draw() {  
+
+  angleMode (DEGREES);
+  rotateX(140); 
+  rotateY(30);
+  angleMode (RADIANS);
+  
+  background('#D0CECF');
+  texture(matchImage);
+  noStroke();
+  scale (60);
+  model (matchbox);
+
+
+
+}
