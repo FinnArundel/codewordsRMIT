f8aed92a18afb5d32717478cbf64ba6d9800e103
FinnArundel <68985116+FinnArundel@users.noreply.github.com>
2020-10-09 23:05:12 +1100

    Add files via upload

---
+++ week_11/week_11_start/class.js
@@ -0,0 +1,63 @@
+class NodePoint {
+  constructor (x, y, z, time) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.time = time;
+  }
+
+  connect () {
+
+    for ( let i = 1; i < words.length; i++) {
+      if (frameCount  > this.time * i + this.time) {
+        stroke (255);
+        strokeWeight (1);
+        line (node[i - 1].x, node[i - 1].y, node[i - 1].z, node[i].x, node[i].y, node[i].z); // connects the words in sentence order
+      }
+    }
+  }
+
+  show (count) {
+    for (let i = 0; i < words.length; i++) {
+      if (frameCount > this.time * i + this.time) {
+
+        if (count === 0) {
+          fill (255,0,0);
+        } else {
+          fill (255);
+        }
+
+        textAlign (CENTER);
+        textFont (helvetica, 12);
+        noStroke();
+        
+        let words = sentences[index].split(" ");
+
+        push();
+        translate (this.x, this.y, this.z);
+        let spin = radians(frameCount) *0.1;
+        rotateY (spin);
+        text(words[count], 0, 0);
+        pop();
+      }
+    }
+  }
+
+  sound () {
+    for (let i = 0; i < words.length; i++) {
+      if (frameCount === this.time * i + this.time) {
+        sound.play();
+      }
+    }
+  }
+
+  move () {
+    val = spaceSlider.value();
+    
+    for (let i = 0; i < words.length; i++) {
+      this.x *= val;
+      this.y *= val;
+      this.z *= val;
+    }
+  }
+}
